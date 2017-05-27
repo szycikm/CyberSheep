e8aa4e524d5b540ede217e145b51cd54cf59bb6d
szycikm <szycikm@gmail.com>
2017-05-27 19:19:48 +0200

    Add Animal

---
+++ Species/Animal.py
@@ -0,0 +1,72 @@
+from Names import Names
+from Species.Organism import Organism, ADULT_AGE
+
+
+class Animal(Organism):
+
+	def __init__(self, fromworld, x, y):
+		self._name = Names.getrandomname()
+		super().__init__(fromworld, x, y)
+
+	def action(self):
+		"""default animal behaviour"""
+		self.move(self.randomizefield())
+
+	def getname(self):
+		return self._name
+
+	def incrementage(self):
+		super().incrementage()
+		if self.getage() == ADULT_AGE:
+			print("%s is all grown up now!" % (self.introduce()))  # TODO write this in UI
+
+	def introduce(self):
+		return "%s %s" % (Names.getspciesname(self._type), self._name)
+
+	def tostring(self):
+		return super().tostring() + ";" + self._name
+
+	def move(self, nextposition):
+		if self.collision(self._fromworld.getorganismbyposition(nextposition)):
+			self._position = nextposition
+			print("%s moved to (%i,%i)" % (self.introduce(), self._position.x, self._position.y))  # TODO write this in UI
+
+	def randomizefield(self):
+		"""just grab the first random field"""
+		return self.randomizefields()[0]
+
+	def collision(self, other):
+		"""default animal collision logic. return value = if animal should be moved or not"""
+		if other is None:
+			return True  # nothing stands here so we might as well go there
+		elif self.gettype() == other.gettype():
+			# animal of the	same type stands here -> just have sex and don't go there
+
+			# pick a place for the baby
+			for coords in self.randomizefields():
+				child = self.clone(self._fromworld, coords)
+				if self._fromworld.addorganism(child):
+					print("D'awww. %s and %s are having a baby! And it's name is %s" % (self.introduce(), other.introduce(), child.getname()))  # TODO write this in UI
+					return False
+
+			# no place for the baby
+			print("%s and %s wanted to have a baby, but the world decided otherwise" % (self.introduce(), other.introduce()))
+			return False
+		else:
+			# stronger (or attacker) wins and takes looser's field
+			if self.getstrength() >= other.getstrength():
+				if other.tryresistattack(self):
+					print("%s resisted %s's attack!" % (other.introduce(), self.introduce()))
+					return False  # other organism reflected attarck -> don't move
+				else:
+					print("Yeah! %s ate %s!" % (self.introduce(), other.introduce()))
+					other.die()  # how dramatic
+					return True  # we just killed organism that standed there -> we now take it's place
+			else:
+				if isinstance(other, Plant):
+					print("Oh no! %s ate %s and DIED!" % (self.introduce(), other.introduce()))  # we just ate a plant TODO write this in UI
+				else:
+					print("Oh no! %s ate %s!" % (other.introduce(), self.introduce()))
+
+				self.die()
+				return False
--- Species/Organism.py
+++ Species/Organism.py
@@ -7,16 +7,16 @@ ADULT_AGE = 5
 
 class Organism:
 
-	def __init__(self, fromworld, x=-1, y=-1):
-		if x == -1 or y == -1:
+	def __init__(self, fromworld, x, y):
+		if x is None or y is None:
 			x = randint(0, fromworld.getmaxxy().x)
 			y = randint(0, fromworld.getmaxxy().y)
 		self._fromworld = fromworld
 		self._position = Coordinates(x, y)
-		self._age = 0
 		self._type = None
 		self._strength = None
 		self._initiative = None
+		self.__age = 0
 		self.__alive = True
 
 	def __lt__(self, other):
@@ -37,7 +37,7 @@ class Organism:
 	def introduce(self):
 		pass
 
-	def tryresistattack(attacker):
+	def tryresistattack(self, attacker):
 		return False
 
 	def gettype(self):
@@ -45,13 +45,13 @@ class Organism:
 
 	def draw(self):
 		"""if organism is below ADULT_AGE turns draw it small. Just for fun."""
-		return self._type.lower() if self._age < ADULT_AGE else self._type
+		return self._type.lower() if self.__age < ADULT_AGE else self._type
 
 	def getxy(self):
 		return self._position
 
 	def getage(self):
-		return self._age
+		return self.__age
 
 	def getstrength(self):
 		return self._strength
@@ -63,7 +63,7 @@ class Organism:
 		return self._initiative
 
 	def incrementage(self):
-		self._age += 1
+		self.__age += 1
 
 	def isalive(self):
 		return self.__alive
@@ -71,10 +71,9 @@ class Organism:
 	def die(self):
 		"""fly, fly, PIZZA DIE!"""
 		self.__alive = False  # don't actually die, just mark organism as dead
-		self.randomizefields()
 
 	def tostring(self):
-		return "%s;%i;%i%i%i%i" % (self._type, self._age, self._strength, self._initiative, self._position.x, self._position.y)
+		return "%s;%i;%i%i%i%i" % (self._type, self.__age, self._strength, self._initiative, self._position.x, self._position.y)
 
 	def randomizefields(self):
 		"""randomizes 2 to 4 new coordinates respecting the world limits"""
